6b43a965b25496ec45f5d327ed5dd5822f968dbb
Tyler D <3713763+tylerd81@users.noreply.github.com>
2019-01-15 19:32:34 -0500

    restarting with a data driven approach

--- lib/read_list_saver.dart
+++
@@ -1,5 +0,0 @@
-import "package:reading_list/read_list.dart";
-
-abstract class ReadListSaver {
-  Future<bool> save(ReadList readList);
-}
--- lib/src/book_progress.dart
+++ lib/src/book_progress.dart
@@ -1,51 +1,7 @@
-import "package:reading_list/read_list.dart";
-
 class BookProgress {
-  // readTimeList is the list of all the times we have read this book
-  List<ReadTime> readTimeList = [];
-  Book book;
-  DateTime dateFinished;
+  DateTime when;
+  int timeRead; //number of seconds
+  int pagesRead;
 
-  bool finished = false;
-
-  // constructor
-  BookProgress(this.book);
-
-  int get percentageComplete {
-    int totalPagesRead = 0;
-
-    if (book.numPages == 0 || readTimeList.isEmpty) {
-      return 0;
-    }
-
-    for (ReadTime rt in readTimeList) {
-      totalPagesRead += rt.numPagesRead;
-    }
-
-    return ((totalPagesRead / book.numPages) * 100).floor();
-  }
-
-  int get totalTimeRead {
-    int sum = 0;
-
-    for (ReadTime rt in readTimeList) {
-      sum += rt.numSeconds;
-    }
-
-    return sum;
-  }
-
-  int get totalPagesRead {
-    int sum = 0;
-
-    for (ReadTime rt in readTimeList) {
-      sum += rt.numPagesRead;
-    }
-
-    return sum;
-  }
-
-  void add(ReadTime readTime) {
-    readTimeList.add(readTime);
-  }
+  BookProgress(this.when, this.timeRead, this.pagesRead);
 }
